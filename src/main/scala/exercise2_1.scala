805c8f104b60ed6a4e9d0e9fa621bb6f2c3de206
sp5t7be9 <sp5t7be9@sp5t7be9-no-MacBook-Pro.local>
2017-07-23 00:41:14 +0900

    first commit

---
+++ src/main/scala/exercise2_1.scala
@@ -0,0 +1,19 @@
+package main.scala
+
+object exercise2_1 {
+
+  def main(args: Array[String]): Unit = {
+    println(fib(10))
+  }
+
+  def fib(n: Int): Int = {
+    def loop(n: Int): Int = {
+      n match {
+        case 0 => 0
+        case 1 => 1
+        case _ => loop(n - 1) + loop (n - 2)
+      }
+    }
+    loop(n)
+  }
+}
