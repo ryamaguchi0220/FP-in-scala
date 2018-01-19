9cd3c0c039f4ef3a956fb6c602c4a6762e87098c
Ryu Yamaguchi <maguchi579@denkiyagi.jp>
2018-01-19 20:48:22 +0900

    Add Exercise3_10 and Exercise3_11

---
+++ src/main/scala/Exercise3_11.scala
@@ -0,0 +1,26 @@
+package main.scala
+
+import scala.collection.immutable.{::, List, Nil}
+
+// Saw nothing
+object Exercise3_11 {
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = {
+    as match {
+      case Nil => z
+      case head :: tail => foldLeft(tail, f(z, head))(f)
+    }
+  }
+
+  def sum(as: List[Int]): Int = {
+    foldLeft(as, 0)(_ + _)
+  }
+
+  def product(as: List[Double]): Double = {
+    foldLeft(as, 0.0)(_ * _)
+  }
+
+  def length[A](as: List[A]): Int = {
+    foldLeft(as, 0)((accumulator, _) => accumulator + 1)
+  }
+}
+
