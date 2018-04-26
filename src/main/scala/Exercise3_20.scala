58571da8f16f4ad502799bba849cdb22c571ab2a
ryamaguchi0220 <maguchi579@gmail.com>
2018-04-26 23:52:31 +0900

    Add Exercise3_20

---
+++ src/main/scala/Exercise3_20.scala
@@ -0,0 +1,11 @@
+package main.scala
+
+object Exercise3_20 {
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = {
+    as match {
+      case Nil => Nil
+      case head :: tail => f(head) ::: flatMap(tail)(f)
+    }
+  }
+}
+
