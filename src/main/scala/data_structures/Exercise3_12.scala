b2cff7962439247c59bb9a6681caa6ff0920afef
y_ryu <maguchi579@denkiyagi.jp>
2018-05-05 17:54:15 +0900

    Add some variations of Exercise3_12 and refactor ‘def reverse’.

--- src/main/scala/data_structures/Exercise3_12.scala
+++ src/main/scala/data_structures/Exercise3_12.scala
@@ -12,8 +12,10 @@ object Exercise3_12 {
   def reverse[A](as: List[A]): List[A] = {
     as match {
       case Nil => Nil
-      case head :: tail => tail.foldLeft(List(head))((b, a) => a :: b)
+      case head :: tail => reverse(tail) ::: List(head)
     }
   }
+  def reverseViaFoldRight[A](as: List[A]): List[A] = as.foldRight(List.empty[A])((a, b) => b ::: List(a))
+  def reverseViaFoldLeft[A](as: List[A]): List[A] = as.foldLeft(List.empty[A])((b, a) => a :: b)
 }
 
