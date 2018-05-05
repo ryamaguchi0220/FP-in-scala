1dab12eaef9ddd7f7526be0710c76c4b23592ad6
y_ryu <maguchi579@denkiyagi.jp>
2018-05-05 20:00:03 +0900

    Add Exercise3_13

---
+++ src/main/scala/data_structures/Exercise3_13.scala
@@ -0,0 +1,15 @@
+package main.scala.data_structures
+
+import scala.collection.immutable.List
+
+object Exercise3_13 {
+  // This can avoid SOF, but O(2N)
+  def foldRightViaFoldLeft[A, B](as: List[A])(z: B)(f: (A, B) => B): B = {
+    as.reverse.foldLeft(z)((b, a) => f(a, b))
+  }
+  
+  def foldLeftViaFoldRight[A, B](as: List[A])(z: B)(f: (B, A) => B): B = {
+    as.reverse.foldRight(z)((a, b) => f(b, a))
+  }
+}
+
