05bfac7ee667e61897412e8dfdb27946007d6509
ryamaguchi0220 <maguchi579@gmail.com>
2018-05-06 22:31:23 +0900

    Refactor Exercise3_5

--- src/main/scala/data_structures/Exercise3_5.scala
+++ src/main/scala/data_structures/Exercise3_5.scala
@@ -6,20 +6,17 @@ import scala.collection.immutable.{::, List, Nil}
 object Exercise3_5 {
 
   def main(args: Array[String]): Unit = {
-    println(s"I expect that ${dropWhile(List(1, 2, 3), (n: Int) => n <= 2)} is List(3)")
-    println(s"I expect that ${dropWhile(List(1, 2, 3), _ => true)} is Nil")
-    println(s"I expect that ${dropWhile(List.empty[Int], _ => true)} is Nil")
+    println(s"I expect that ${dropWhile(List(1, 2, 3))(_ <= 2)} is List(3)")
+    println(s"I expect that ${dropWhile(List(1, 2, 3))(_ => true)} is Nil")
+    println(s"I expect that ${dropWhile(List.empty[Int])(_ => true)} is Nil")
   }
 
-  def dropWhile[A](list: List[A], f: A => Boolean): List[A] = {
-    @tailrec
-    def loop(l: List[A], f: A => Boolean): List[A] = {
-      l match {
-        case Nil => Nil
-        case head :: tail => if(f(head)) loop(tail, f) else l
-      }
+  @tailrec
+  def dropWhile[A](list: List[A])(predicate: A => Boolean): List[A] = {
+    list match {
+      case Nil => Nil
+      case head :: tail => if(predicate(head)) dropWhile(tail)(predicate) else list
     }
-    loop(list, f)
   }
 }
 
