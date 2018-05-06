a56faa1d51dfcdaab215a2c863e36d796ac833c9
ryamaguchi0220 <maguchi579@gmail.com>
2018-05-06 22:42:24 +0900

    Refactor Exercise3_4

--- src/main/scala/data_structures/Exercise3_4.scala
+++ src/main/scala/data_structures/Exercise3_4.scala
@@ -11,16 +11,13 @@ object Exercise3_4 {
     println(s"I expect that ${drop(List.empty[Int], 3)} is Nil")
   }
 
+  @tailrec
   def drop[A](list: List[A], num: Int): List[A] = {
-    @tailrec
-    def loop(l: List[A], n: Int): List[A] = {
-      (l, n) match {
-        case (_, 0) => l
-        case (Nil, _) => Nil
-        case (_ :: tail, n) => loop(tail, n - 1)
-      }
+    (list, num) match {
+      case (Nil, _) => Nil
+      case (_, n) if (n <= 0) => list
+      case (_ :: tail, n) => drop(tail, n - 1)
     }
-    loop(list, num)
   }
 }
 
