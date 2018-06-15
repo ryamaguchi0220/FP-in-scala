6749944fb4237870dd852b62ddb3b66ea1200f2d
y_ryu <maguchi579@denkiyagi.jp>
2018-06-15 21:38:39 +0900

    Add some exercises

---
+++ src/main/scala/laziness/Exercise5_12.scala
@@ -0,0 +1,31 @@
+package main.scala.laziness
+
+object Exercise5_12 {
+  sealed trait Stream[+A]
+  case object Empty extends Stream[Nothing]
+  case class Cons[+A](head: () => A, tail: () => Stream[A]) extends Stream[A]
+
+  object Stream {
+    def cons[A](head: => A, tail: => Stream[A]): Stream[A] = {
+      lazy val lazyHead = head
+      lazy val lazyTail = tail
+      Cons(() => lazyHead, () => lazyTail)
+    }
+    def empty[A]: Stream[A] = Empty
+    def apply[A](as: A*): Stream[A] = if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+    def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = {
+      @annotation.tailrec
+      def loop(as: Stream[A], lz: S): Stream[A] = {
+        f(lz) match {
+          case Some((a, s)) => loop(cons(a, as), s)
+          case None => as
+        }
+      }
+      loop(empty, z)
+    }
+    def fibsViaUnfold(): Stream[Int] = unfold((0, 1)) { case (pre, i) => Some((pre, (i, pre + i))) }
+    def fromViaUnfold(n: Int): Stream[Int] = unfold(n)(s => Some((s, s + 1)))
+    def constantViaUnfold[A](a: A): Stream[A] = unfold(a)(s => Some((s, s)))
+    val onesViaUnfold: Stream[Int] = unfold(1)(s => Some((s, s)))
+  }
+}
