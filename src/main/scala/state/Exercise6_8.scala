c758d2dc05b06add508963d2fb85c06a73ea2149
ryamaguchi0220 <maguchi579@gmail.com>
2018-07-15 23:57:46 +0900

    Update Exercise6_8

--- src/main/scala/state/Exercise6_8.scala
+++ src/main/scala/state/Exercise6_8.scala
@@ -1,12 +1,19 @@
 package main.scala.state
 
+import Exercise6_1.nonNegativeInt
 import RNG._
 
 object Exercise6_8 {
-  def flatMap[A, B](f: Rand[A]])(g: A => Rand[B]): Rand[B] = {
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = {
     r => {
       val (a, nextRng) = f(r)
       g(a)(nextRng)
+    }
+  }
+  def nonNegativeLessThan(n: Int): Rand[Int] = {
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) rng => (mod, rng) else nonNegativeLessThan(n)
     }
   }
 }
