116c03d14de51062913d00b462404e838e2be5ea
ryamaguchi0220 <maguchi579@gmail.com>
2018-07-22 23:58:10 +0900

    Refactor Exercise10_3

--- src/main/scala/monoid/Exercise10_3.scala
+++ src/main/scala/monoid/Exercise10_3.scala
@@ -6,20 +6,20 @@ object Exercise10_3 {
       (a, b) match {
         case (Some(a), None) => Some(a)
         case (None, Some(b)) => Some(b)
-        case (Some(a), Some(b)) => Some(a)
+        case (Some(a), Some(_)) => Some(a)
         case (None, None) => None
       } 
     }
     override def empty: Option[A] = None
   }
-   def optionMonoid2[A] = new Monoid[Option[A]] {
+  def optionMonoid2[A] = new Monoid[Option[A]] {
     override def combine(a: Option[A], b: Option[A]): Option[A] = {
       (a, b) match {
         case (Some(a), None) => Some(a)
         case (None, Some(b)) => Some(b)
-        case (Some(a), Some(b)) => Some(b)
+        case (Some(_), Some(b)) => Some(b)
         case (None, None) => None
-      } 
+      }
     }
     override def empty: Option[A] = None
   }
