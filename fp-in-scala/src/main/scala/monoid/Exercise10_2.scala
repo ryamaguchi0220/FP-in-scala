dbfec8e15e984d3e869e7924d860f0f954d0b8b1
ryamaguchi0220 <maguchi579@gmail.com>
2018-09-08 23:43:04 +0900

    Rename two monoids

--- fp-in-scala/src/main/scala/monoid/Exercise10_2.scala
+++ fp-in-scala/src/main/scala/monoid/Exercise10_2.scala
@@ -1,11 +1,11 @@
 package main.scala.monoid
 
 object Exercise10_2 {
-  def endoMonoid1[A] = new Monoid[A => A] {
+  def endoAndThenMonoid[A] = new Monoid[A => A] {
     override def combine(a: A => A, b: A => A): A => A = ((x: A) => b(a(x)))
     override def empty: A => A = ((x: A) => x)
   }
-  def endoMonoid[A] = new Monoid[A => A] {
+  def endoComposeMonoid[A] = new Monoid[A => A] {
     override def combine(a: A => A, b: A => A): A => A = ((x: A) => a(b(x)))
     override def empty: A => A = ((x: A) => x)
   }
